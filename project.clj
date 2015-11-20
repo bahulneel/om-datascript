edf2b2d516e4c5988affb08add21b4bda393c5b7
Neel Upadhyaya <bahulneel@gmail.com>
2015-11-20 10:30:01 +0000

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-datascript "0.0.3"
+(defproject om-datascript "0.0.4"
   :description "Make om and datascript play nice"
   :url "http://github.com/bahulneel/om-datascript"
   :license {:name "Eclipse Public License - v 1.0"
